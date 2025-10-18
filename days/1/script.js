a14ea266a8f8d5ae1a7eeea484a77d91ec58d51e
csecsoliver <0oliogamer0@gmail.com>
2025-10-18 08:44:26 +0200

    something messed up

--- days/1/script.js
+++ days/1/script.js
@@ -1,1 +1,14 @@
-let file = fetch("./input1.txt")
+async function main(){
+    let file_response = await fetch("./input1.txt");
+    let file_string = await file_request.text();
+    let row_list = file_string.split("\n")
+    let col1 = [];
+    let col2 = [];
+    for (i of row_list) {
+      line = i.split(" ");
+      col1 += line[0];
+      col2 += line[-1];
+    }
+    
+}
+main();
